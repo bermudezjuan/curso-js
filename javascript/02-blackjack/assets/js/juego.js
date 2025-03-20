08bf3df78902c95f13cd9cbb0ff99b4a8f7fd572
Juan Bermudez <jbermudez@cinesunidos.com>
2025-03-20 09:21:24 -0400

    Baraja lista

---
+++ javascript/02-blackjack/assets/js/juego.js
@@ -0,0 +1,51 @@
+/**
+    2C = Two of Clubs
+    2D = Two of Diamonds
+    2H = Two of Hearts
+    2S = Two of Spades
+ */
+
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K'];
+
+// Esta función crea un deck
+const crearDeck = () => {
+    for (let i = 2; i <= 10; i++) {
+      for (let tipo of tipos) {
+        deck.push(i + tipo);
+      }        
+    }
+    
+    for (let tipo of tipos) {
+        for (let esp of especiales) {
+            deck.push(esp + tipo);
+        }
+    }        
+    
+    deck = _.shuffle( deck );
+    console.log( deck );
+    return deck;
+}
+
+crearDeck();
+
+// Esta función me permite tomar una carta
+const pedirCarta = () => {
+
+    if(deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop();    
+    return carta;
+}
+
+const valorCarta = ( carta ) => {
+    const valor = carta.substring(0, carta.length - 1);   
+    return ( isNaN( valor ) ) ? 
+            ( valor === 'A' ) ? 11 : 10 
+            : valor * 1;
+}
+
+let valor = valorCarta(pedirCarta());
+console.log({ valor });
