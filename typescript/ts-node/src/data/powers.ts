197ba795750dd653e8207850db663adba94a49f2
Juan Bermudez <jbermudez@cinesunidos.com>
2025-05-26 14:53:33 -0400

    Fin Sección 10

---
+++ typescript/ts-node/src/data/powers.ts
@@ -0,0 +1,17 @@
+export interface Power {
+    id: Number;
+    desc: String;
+}
+
+const powers: Power[] = [
+    {
+        id: 1, 
+        desc: 'Money'
+    },
+    {
+        id: 2, 
+        desc: 'Drugs'
+    },    
+]; 
+
+export default powers;
---
+++ typescript/ts-node/src/index.ts
@@ -0,0 +1,8 @@
+// import { Hero as SuperHero } from './classes/Hero'
+import { Hero } from './classes/Hero'
+import powers from './data/powers';
+
+const ironman = new Hero('Ironman', 1, 55);
+console.log( ironman );
+
+console.log( ironman.power );
