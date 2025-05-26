c47f84eb9eff820c8acc21051a4217c80862ebc6
Juan Bermudez <jbermudez@cinesunidos.com>
2025-05-26 16:03:52 -0400

    Fix

--- typescript/ts-node/src/index.ts
+++ typescript/ts-node/src/index.ts
@@ -1,6 +1,5 @@
 // import { Hero as SuperHero } from './classes/Hero'
 import { Hero } from './classes/Hero'
-import powers from './data/powers';
 
 const ironman = new Hero('Ironman', 1, 55);
 console.log( ironman );
