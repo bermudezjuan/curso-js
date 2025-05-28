1d54d859a818a852878e3a299dd7d5e7ce263514
Juan Bermudez <jbermudez@cinesunidos.com>
2025-05-28 09:50:24 -0400

    Curso Finalizado

--- typescript/ts-node/src/index.ts
+++ typescript/ts-node/src/index.ts
@@ -1,6 +1,10 @@
-import { getPokemon } from "./generics/get-pokemon";
+import { Pokemon } from "./decorators/pokemon-class";
 
-getPokemon(1)
-.then( pokemon => console.log( pokemon.sprites.front_default ) )
-.catch( error => console.error( error ) )
-.finally( () => console.log( 'Fin de getPokemon.' ) );
+const charmander = new Pokemon("Charmander");
+
+// (Pokemon.prototype.customName as any) = 'Pikachu';
+
+// console.log(charmander.savePokemonToDB(50));
+// charmander.savePokemonToDB(10);
+// charmander.publicApi = 'https://hola-mundo.com';
+console.log(charmander);
